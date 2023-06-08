a33ee7c2a6e2bd3eafe498d568038333befc0cbd
Anuja Ajay <anujajay10@gmail.com>
2023-06-08 09:13:19 +0530

    first commit

---
+++ RabbitmqEmailNotification-master/src/main/java/com/UST/RabbitmqEmailNotification/RabbitmqEmailNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.UST.RabbitmqEmailNotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqEmailNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqEmailNotificationApplication.class, args);
+	}
+
+}
---
+++ RabbitmqEmailNotification-master/src/test/java/com/UST/RabbitmqEmailNotification/RabbitmqEmailNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UST.RabbitmqEmailNotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqEmailNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
